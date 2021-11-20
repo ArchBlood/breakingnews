2d5a05039fc98687595db96a772ad83e7c1b21af
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2021-11-20 17:32:12 -0500

    Enh: New Richtext Editor

--- views/index/index.php
+++ views/index/index.php
@@ -1,13 +1,20 @@
+<?php
+
+use humhub\modules\content\widgets\richtext\RichText;
+
+/* @var $this \humhub\modules\ui\view\components\View */
+
+?>
 <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-    <h4 class="modal-title" id="myModalLabel"><?php echo $title; ?></h4>
+    <h4 class="modal-title" id="myModalLabel"><?= $title; ?></h4>
 </div>
 <div class="modal-body">
-    <?php echo humhub\widgets\MarkdownView::widget(array('markdown' => $message)); ?>
+<?= RichText::output($model->message); ?>
 </div>
 <div class="modal-footer">
     <button type="button" class="btn btn-primary"
-            data-dismiss="modal"><?php echo Yii::t('BreakingnewsModule.views_index', 'Close'); ?></button>
+            data-dismiss="modal"><?= Yii::t('BreakingnewsModule.views_index', 'Close'); ?></button>
 </div>
 
 
